c0f37d7e293353a4fcf518f15fdceb8b075c24da
Lee <leeblazek@gmail.com>
2014-06-28 15:32:15 +1200

    added copy html from dev folder to production

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHtml = require('gulp-minify-html'),
+
     concat = require('gulp-concat');
 
 var env,
@@ -53,6 +55,7 @@ gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
+        .pipe(gulpif(env === 'production', uglify()))
         .pipe(gulp.dest(outputDir + 'js'))
         .pipe(connect.reload())
 });
@@ -81,13 +84,15 @@ gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js'])
     gulp.watch('components/sass/*.scss', ['compass'])
-    gulp.watch(htmlSources, ['html'])
+    gulp.watch('builds/development/*.html', ['html'])
     gulp.watch(htmlSources, ['json'])
 
 })
 
 gulp.task('html', function() {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+        .pipe(gulpif(env === 'production', minifyHtml()))
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
         .pipe(connect.reload())
 })
 
